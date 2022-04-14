395994a9bc8bb82301a57bf68d3a9f536322cd4c
ParvezHossain <parvezhossain724@gmail.com>
2022-04-14 14:48:02 +0600

    Basic graphql configuration has been set up

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+// it exposes a middleware function which will eventually use by express middleware
+const { graphqlHTTP } = require("express-graphql");
+
+const { buildSchema } = require("graphql");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+        type RootMutation {
+            createEvent(name: String): String
+        }
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+      events: () => {
+        return ["Hello", "1", "2"];
+      },
+      createEvent: (args) => {
+        const eventName = args.name;
+        return eventName;
+      },
+    },
+    graphiql: true,
+  }),
+);
+
+app.listen(3000, () => {
+  console.log(`Server is listening on port: 3000`);
+});
